764345b06592de35e477d9bc996be907e32d96e5
Nickolas "Nick" Manes <59900634+manesn@users.noreply.github.com>
2020-02-27 02:48:48 -0500

    Finished assignment + Read Desc
    Two things about this. The "notsidious.ogg" in this Godot project gets cut off when the project gets played; probably because of how long it is or maybe it's just too large, but previewing the sound in the editing mode/outside of game mode works.
    
    The music has a large gap of silence in the beginnning; I couldn't slim this down, or at least I couldn't in Musescore. There's a song there, I promise, and it's adequate, not some jumble of notes.

---
+++ Sound1.gd
@@ -0,0 +1,12 @@
+extends AudioStreamPlayer
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Check the sound file. It's getting cut off whenever it gets played, for some reason. Probably my favorite of the three sounds to record/create, using my voice and some inspiration from Emperor Palpatine.
